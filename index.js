6eb82f8276131aa04985f48b1d74e020133e22e5
Micah Silverman <micah@afitnerd.com>
2025-03-16 22:38:00 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }  
+})();
